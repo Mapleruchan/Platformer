840da671f2849685fd24545b317e2b3d5fcefe93
Mapleruchan <134357878+Mapleruchan@users.noreply.github.com>
2023-06-27 15:16:31 +0800

    astolfo is a trap

--- Assets/scripts/player move.cs
+++ Assets/scripts/player move.cs
@@ -9,6 +9,7 @@ public class playermove : MonoBehaviour
     private Rigidbody2D rigidBody;
     private Vector2 movementInput;
     private Animator anim;
+    public int cash;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,41 +20,44 @@ public class playermove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            anim.enabled = true;
-            anim.SetTrigger("back anim");
+       //if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+       // {
+       //     anim.enabled = true;
+       //     anim.SetTrigger("back anim");
             
-        }
+       // }
  
 
-        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            anim.enabled = true;
-            anim.SetTrigger("forward anim");
+       // if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+       // {
+       //     anim.enabled = true;
+       //     anim.SetTrigger("forward anim");
             
-        }
+       // }
     
 
-        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            anim.enabled = true;
-            anim.SetTrigger("leftanim");
+       // if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+       // {
+       //     anim.enabled = true;
+       //     anim.SetTrigger("leftanim");
             
-        }
+       // }
     
 
-        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            anim.enabled = true;
-            anim.SetTrigger("rightanim");
-        }   
+       // if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+       // {
+       //     anim.enabled = true;
+       //     anim.SetTrigger("rightanim");
+       // }   
             
 
-        if (Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.A))
-        {
-            anim.enabled= false;
-        }
+       // if (Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.A))
+       // {
+       //     anim.enabled= false;
+       // }
+       anim.SetFloat("horizontal", movementInput.x);
+       anim.SetFloat("vertical", movementInput.y);
+        anim.SetFloat("sped",movementInput.sqrMagnitude);
     }
     private void FixedUpdate()
     {
@@ -68,5 +72,13 @@ public class playermove : MonoBehaviour
     movementInput = inputValue.Get<Vector2>();
 
 }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if ( collision.gameObject.CompareTag("coins"))
+        {
+            Destroy(collision.gameObject);
+            cash++;
+        }
+    }
 
 }
